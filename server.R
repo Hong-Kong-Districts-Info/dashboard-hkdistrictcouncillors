d66f8cc43b5e5f0724b4633efef3e21a2dacfb1b
Martin Chan <martinchan53@gmail.com>
2020-07-05 10:23:07 +0100

    feat: Add sheet URL to server.R

--- server.R
+++ server.R
@@ -10,7 +10,8 @@
 server <- function(input, output, session) {
   
   ## Read data from Google Sheets 4
+  sheet_url <- "https://docs.google.com/spreadsheets/d/1usk9Q-5lA4bL_z6KXpUohc_2x_KhDgLxtm-YEtim_yk/"
   # data_master_raw <-
-  #   googlesheets4::read_sheet(ss = "https://docs.google.com/spreadsheets/d/1usk9Q-5lA4bL_z6KXpUohc_2x_KhDgLxtm-YEtim_yk/",
-  #                             sheet = "master")
+  #   googlesheets4::read_sheet(ss = sheet_url,
+  #                             sheet = "Master")
 }
