9a61b068906bfba37cb7bc69218b6037f518ee2e
avisionh <a_vision@hotmail.co.uk>
2020-07-19 11:45:12 +0100

    fix: Change typo from ZN to EN
    This is because it is the English language version of the Chinese name.

--- .development/googlesheets_dataclean.R
+++ .development/googlesheets_dataclean.R
@@ -19,7 +19,7 @@ clean_gsheet_dat <- function(data){
     clean_names() %>%
     separate(col = xuan_qu_constituency, into = c("Constituency_ZH", "Constituency_EN"), sep = "\n", extra = "merge") %>%
     separate(col = dang_pai_party, into = c("Party_ZH", "Party_EN"), sep = "\n", extra = "merge") %>%
-    separate(col = qu_yi_yuan_dc, into = c("DC_ZH", "DC_ZN"), sep = "\n", extra = "merge") %>%
+    separate(col = qu_yi_yuan_dc, into = c("DC_ZH", "DC_EN"), sep = "\n", extra = "merge") %>%
     rename(ConstituencyCode = "xuan_qu_hao_ma_constituency_code") %>%
     mutate(DropDownText = paste0(ConstituencyCode, ": ", Constituency_ZH, " / ", Constituency_EN))
 }
