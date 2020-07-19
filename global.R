910451e3e427c31d649167150ed7bee1b287cd19
avisionh <a_vision@hotmail.co.uk>
2020-07-19 11:22:58 +0100

    chore: Deauthorise Google Sheets
    This is so we do not need to pass in our personal credentials to access the Google Sheet which is publicly available. It allows the app to be deployed without a personal Google account.

--- global.R
+++ global.R
@@ -23,6 +23,10 @@ library(DT)
 ## Read data from Google Sheets 4
 sheet_url <- "https://docs.google.com/spreadsheets/d/1usk9Q-5lA4bL_z6KXpUohc_2x_KhDgLxtm-YEtim_yk/"
 
+# put gsheets into de-authorised state
+# so no need to provide personal token
+gs4_deauth()
+
 data_master_raw <-
   googlesheets4::read_sheet(ss = sheet_url,
                             sheet = "Master")
