c68a71061e25be2add4a38793bcdcca3c6b95481
Martin Chan <martinchan53@gmail.com>
2020-07-23 21:49:05 +0100

    feat: add links to email and GitHub
    - Logo files also added under www

--- ui.R
+++ ui.R
@@ -13,7 +13,19 @@ ui <- dashboardPage(
   
   # Header
   header = dashboardHeader(
-    title = "HK: District Councillors"
+    title = "HK: District Councillors",
+    tags$li(a(href = 'https://github.com/avisionh/dashboard-hkdistrictcouncillors/',
+              icon("github"),
+              title = "GitHub"),
+            class = "dropdown"),
+    tags$li(a(href = 'mailto: hkdistricts.info@gmail.com',
+              icon("envelope"),
+              title = "Email us"),
+            class = "dropdown"),
+    tags$li(a(href = 'https://hkdistricts-info.shinyapps.io/dashboard-hkdistrictcouncillors/',
+              img(src = 'HKDC-white.png', title = "Back to Home", height = "30px"),
+              style = "padding-top:10px; padding-bottom:10px;"),
+            class = "dropdown")
   ),
   
   # Sidebar
