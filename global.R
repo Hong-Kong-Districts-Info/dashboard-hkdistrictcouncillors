0f58d437643a2b3a1341dfdc636f9a17c17b749e
avisionh <a_vision@hotmail.co.uk>
2020-07-25 11:51:17 +0100

    feat: Import shapesfile of districts
    This is so we can start mapping them.

--- global.R
+++ global.R
@@ -23,8 +23,9 @@ library(shinydashboard)
 library(DT)
 
 
-## Read data from Google Sheets 4
+## data source
 sheet_url <- "https://docs.google.com/spreadsheets/d/1007RLMHSukSJ5OfCcDJdnJW5QMZyS2P-81fe7utCZwk/"
+path_shape <- "data/DCCA_2019.shp"
 
 # put gsheets into de-authorised state
 # so no need to provide personal token
@@ -35,4 +36,47 @@ data_master_raw <-
                             sheet = "Master",
                             na = c("N/A", "")) %>% 
   # infoBox colour indicating if FB link exists
+<<<<<<< HEAD
   mutate(exists_fb = if_else(condition = !is.na(x = facebook), "blue", "black"))
+||||||| constructed merge base
+  mutate(exists_fb = if_else(condition = !is.na(x = facebook), "blue", "black"))
+
+# Version to be shown with DT on app
+data_master_details <-
+  googlesheets4::read_sheet(ss = sheet_url,
+                            sheet = "Master",
+                            na = c("N/A", "")) %>%
+  select(Constituency = "DropDownText",
+         Party,
+         DC,
+         Region,
+         District,
+         FB = "facebook")
+  
+=======
+  mutate(exists_fb = if_else(condition = !is.na(x = facebook), "blue", "black"))
+
+# Version to be shown with DT on app
+data_master_details <-
+  googlesheets4::read_sheet(ss = sheet_url,
+                            sheet = "Master",
+                            na = c("N/A", "")) %>%
+  select(Constituency = "DropDownText",
+         Party,
+         DC,
+         Region,
+         District,
+         FB = "facebook")
+
+# map
+shape_district <- readOGR(dsn = path_shape)
+## convert to dataframe
+shape_district <- fortify(model = shape_district)
+
+## prepare for chloropleth
+centroids <- setNames(object = do.call(what = "rbind.data.frame",
+                                      args = by(shape_district, shape_district$group, function(x) {Polygon(x[c('long', 'lat')])@labpt})),
+                      c('long', 'lat'))
+centroids$label <- shape_district$id[match(rownames(centroids), shape_district$group)]
+  
+>>>>>>> feat: Import shapesfile of districts
