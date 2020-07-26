d4e264ce1c06db9c0f450081962b14131ef72be0
avisionh <a_vision@hotmail.co.uk>
2020-07-26 17:21:55 +0100

    refactor: Clean up rebase
    Also join shape file to GSheet so carrying less dataframes around.

--- global.R
+++ global.R
@@ -22,10 +22,10 @@ library(shinyBS)
 library(shinydashboard)
 library(DT)
 
-# put gsheets into de-authorised state so no need for personal token for app
+## put gsheets into de-authorised state so no need for personal token for app
 gs4_deauth()
 
-# set theme for plots
+## set theme for plots
 plot_theme <- theme(plot.title = element_text(face = "bold", hjust = 0.5),
                     plot.subtitle = element_text(face = "bold", hjust = 0.5),
                     panel.background = element_blank(),
@@ -41,88 +41,26 @@ path_shape_hk <- "data/gadm/gadm36_HKG_0.shp"
 
 # Data import -------------------------------------------------------------
 
+## shapefiles
+shape_district <- st_read(dsn = path_shape_district)
+shape_hk <- st_read(dsn = path_shape_hk)
+
 ## gsheets
 data_master_raw <-
   googlesheets4::read_sheet(ss = sheet_url,
                             sheet = "Master",
                             na = c("N/A", "")) %>% 
   # infoBox colour indicating if FB link exists
-<<<<<<< HEAD
-  mutate(exists_fb = if_else(condition = !is.na(x = facebook), "blue", "black"))
-||||||| constructed merge base
-  mutate(exists_fb = if_else(condition = !is.na(x = facebook), "blue", "black"))
-
-# Version to be shown with DT on app
-data_master_details <-
-  googlesheets4::read_sheet(ss = sheet_url,
-                            sheet = "Master",
-                            na = c("N/A", "")) %>%
-  select(Constituency = "DropDownText",
-         Party,
-         DC,
-         Region,
-         District,
-         FB = "facebook")
-  
-=======
-  mutate(exists_fb = if_else(condition = !is.na(x = facebook), "blue", "black"))
-
-# Version to be shown with DT on app
-data_master_details <-
-  googlesheets4::read_sheet(ss = sheet_url,
-                            sheet = "Master",
-                            na = c("N/A", "")) %>%
-  select(Constituency = "DropDownText",
-         Party,
-         DC,
-         Region,
-         District,
-         FB = "facebook")
-
-<<<<<<< HEAD
-# map
-shape_district <- readOGR(dsn = path_shape)
-## convert to dataframe
-shape_district <- fortify(model = shape_district)
-
-## prepare for chloropleth
-centroids <- setNames(object = do.call(what = "rbind.data.frame",
-                                      args = by(shape_district, shape_district$group, function(x) {Polygon(x[c('long', 'lat')])@labpt})),
-                      c('long', 'lat'))
-centroids$label <- shape_district$id[match(rownames(centroids), shape_district$group)]
-  
->>>>>>> feat: Import shapesfile of districts
-||||||| constructed merge base
-# map
-shape_district <- readOGR(dsn = path_shape)
-## convert to dataframe
-shape_district <- fortify(model = shape_district)
-
-## prepare for chloropleth
-centroids <- setNames(object = do.call(what = "rbind.data.frame",
-                                      args = by(shape_district, shape_district$group, function(x) {Polygon(x[c('long', 'lat')])@labpt})),
-                      c('long', 'lat'))
-centroids$label <- shape_district$id[match(rownames(centroids), shape_district$group)]
-  
-=======
-## shapefiles
-shape_district <- st_read(dsn = path_shape_district)
-shape_hk <- st_read(dsn = path_shape_hk)
-
-
-data_master <- data_master_raw %>% 
-  mutate(ConstituencyCode = gsub(x = ConstituencyCode, pattern = "-", replacement = "")) %>% 
+  mutate(exists_fb = if_else(condition = !is.na(x = facebook), "blue", "black"),
+         # remove hyphen for joining to shape file
+         ConstituencyCode = gsub(x = ConstituencyCode, pattern = "-", replacement = "")) %>% 
   left_join(y = shape_district, by = c("ConstituencyCode" = "CACODE"))
 
-# Pre-load/create map
-<<<<<<< HEAD
-map_hk_districts <- 
->>>>>>> feat: Load in HK and district shapes
-||||||| constructed merge base
-map_hk_districts <- 
-=======
+
+# Map import --------------------------------------------------------------
+
+## Pre-load/create map
 map_hk_districts <- ggplot() +
   geom_sf(data = shape_hk, fill = '#009E73') +
   geom_sf(data = shape_district, fill = '#56B4E9', alpha = 0.2, linetype = 'dotted', size = 0.2) +
   plot_theme
->>>>>>> feat: Make lines thinner
