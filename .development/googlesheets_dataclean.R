e3bcf945ce3faeb9c625c10afd52ba37661b8cc9
avisionh <a_vision@hotmail.co.uk>
2020-07-28 23:41:56 +0100

    refactor: Call in function from separate script
    This is so we do not have to define it in the script.

--- .development/googlesheets_dataclean.R
+++ .development/googlesheets_dataclean.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(googlesheets4)
 
 sheet_url <- "https://docs.google.com/spreadsheets/d/1007RLMHSukSJ5OfCcDJdnJW5QMZyS2P-81fe7utCZwk/"
+source('.development/func_clean_gsheet_data.R')
 
 # authorise Google account via prompt
 gs4_auth()
@@ -31,26 +32,12 @@ dc_key <-
   mutate(Region = paste(Region_ZH, "/", Region_EN),
          District = paste(District_ZH, "/", District_EN))
 
-# clean googlesheets ------------------------------------------------------
-
-clean_gsheet_data <- function(data){
-  data %>%
-    clean_names() %>%
-    separate(col = xuan_qu_constituency, into = c("Constituency_ZH", "Constituency_EN"), sep = "\n", extra = "merge") %>%
-    separate(col = dang_pai_party, into = c("Party_ZH", "Party_EN"), sep = "\n", extra = "merge") %>%
-    separate(col = qu_yi_yuan_dc, into = c("DC_ZH", "DC_EN"), sep = "\n", extra = "merge") %>%
-    rename(ConstituencyCode = "xuan_qu_hao_ma_constituency_code") %>%
-    mutate(DropDownText = paste0(ConstituencyCode, ": ", Constituency_ZH, " / ", Constituency_EN),
-           Party = paste(Party_ZH, "/", Party_EN),
-           DC = paste(DC_ZH, "/", DC_EN))
-}
-
 
 # bind list and assign to master ------------------------------------------
 
 master_sheet <-
   list_data %>%
-  map(.f = clean_gsheet_data) %>%
+  map(.f = func_clean_gsheet_data) %>%
   bind_rows() %>%
   mutate(Code = substr(ConstituencyCode, start = 1, stop = 1)) %>%
   left_join(dc_key, by = "Code") %>%
