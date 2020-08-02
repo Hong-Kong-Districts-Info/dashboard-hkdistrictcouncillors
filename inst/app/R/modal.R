5ec0bbb9b20208de2369ff7ad417caa534d0fcd8
avisionh <a_vision@hotmail.co.uk>
2020-08-02 19:42:34 +0100

    feat: Add logo and centre title
    This is so can get more branding into the app.

--- inst/app/R/modal.R
+++ inst/app/R/modal.R
@@ -3,9 +3,11 @@ modal <- function(failed = FALSE) {
   
   modalDialog(
     
-    title = tags$b("Terms of Use"),
+    title = div(style = "text-align: center;",
+                img(src = "logo.png", height = "100px"),
+                p(strong("Terms of Use"))),
     
-    "You understand that the following applies to this website:", br(), br(),
+    p("You understand that the following applies to this website:"), br(),
     tags$ul(
       tags$li("We are not affiliated to any political individuals nor movements."), br(),
       tags$li("We are not liable for any potential misuses."), br(),
@@ -21,8 +23,8 @@ modal <- function(failed = FALSE) {
         text-align: center;",
         radioButtons(inputId = "button_cookieconsent",
                      label = "Do you want to proceed and have tracking cookies enabled?",
-                     choices = c("Yes", "No"),
-                     selected = "No",
+                     choices = c("Yes, I accept tracking", "No, I wish to exit the website"),
+                     selected = "No, I wish to exit the website",
                      inline = TRUE)
     ),
     
