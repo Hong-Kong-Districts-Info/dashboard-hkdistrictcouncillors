40247b08df933f3a631c18e8d5f5cd0dea4f2ac4
avisionh <a_vision@hotmail.co.uk>
2020-08-09 14:11:21 +0100

    feat: Add pop-up to show user-selection
    This is to help them explicitly see their district.

--- inst/app/server.R
+++ inst/app/server.R
@@ -129,6 +129,9 @@ server <- function(input, output, session) {
                     color = '#D55E00',
                     weight = 0.5,
                     fillOpacity = 0.6) %>% 
+        addPopups(lng = react_district_highlight()$centroids[,"X"],
+                  lat = react_district_highlight()$centroids[,"Y"], 
+                  popup = react_district_highlight()$District) %>% 
         # centre map on user-chosen district
         setView(lng = react_district_highlight()$centroids[,"X"],
                 lat = react_district_highlight()$centroids[,"Y"], 
