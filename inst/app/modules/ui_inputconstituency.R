181653db0ccc92097d568363961f4d2e74fe17b5
avisionh <a_vision@hotmail.co.uk>
2020-08-22 15:52:57 +0100

    feat: Pass in translation to text
    This is so we can see the translation in action.

--- inst/app/modules/ui_inputconstituency.R
+++ inst/app/modules/ui_inputconstituency.R
@@ -1,7 +1,7 @@
 output$ui_inputconstituency <- renderUI(
   expr = {
     selectizeInput(inputId = "input_constituency",
-                   label = "Please type or select a constituency",
+                   label = lang$t("Please type or select a constituency"),
                    choices = react_region_dropdown()$DropDownText,
                    selected = "A01: 中環 / CHUNG WAN")
   }
