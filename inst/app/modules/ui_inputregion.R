8ba697194582b60919cd51cc65dba4f6d6a915f3
avisionh <a_vision@hotmail.co.uk>
2020-08-31 17:09:05 +0100

    feat: Add translation for Overview of a DC tab
    This is to provide a better UX.

--- inst/app/modules/ui_inputregion.R
+++ inst/app/modules/ui_inputregion.R
@@ -1,7 +1,7 @@
 output$ui_inputregion <- renderUI(
   expr = {
     selectizeInput(inputId = "input_region",
-                   label = "請選擇或輸入地區 / Please type or select a district",
+                   label = react_lang()$t("Please type or select a district"),
                    choices = c("全部 / All", unique(data_master_raw$District)),
                    selected = "全部 / All") %>% 
       helper(type = "markdown",
